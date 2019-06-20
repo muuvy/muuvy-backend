98753291404b13474b4dd755079f3f7bc240fc98
xXx-DoMoDo-xXx <43957849+xXx-DoMoDo-xXx@users.noreply.github.com>
2019-06-20 18:13:29 +0200

    logging function now

--- src/main/java/org/muuvy/backend/hello/HelloWorldEndpoint.java
+++ src/main/java/org/muuvy/backend/hello/HelloWorldEndpoint.java
@@ -1,5 +1,6 @@
 package org.muuvy.backend.hello;
 
+import org.jboss.logging.Logger;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -9,10 +10,18 @@ import javax.ws.rs.Produces;
 
 @Path("/hello")
 public class HelloWorldEndpoint {
+	private static final Logger LOG = Logger.getLogger(HelloWorldEndpoint.class);
+
 
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
+		LOG.info("Hello World");
+		LOG.warn("WaRnInG");
+		LOG.error("HELLO ERROR WORLD");
+		LOG.fatal("World is fatal");
+		LOG.trace("TRACE WoRlD");
+		LOG.debug("DeBuG WoRlD");
 		return Response.ok("Hello from Thorntail!").build();
 	}
 
