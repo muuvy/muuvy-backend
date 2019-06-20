adef58adcdfb0e4ed74fcf2890e2256c97d4fc31
Mbertschi <michibertschi@gmail.com>
2019-06-20 19:59:08 +0200

    add loging to service

--- src/main/java/org/muuvy/backend/business/rest/FavouriteController.java
+++ src/main/java/org/muuvy/backend/business/rest/FavouriteController.java
@@ -2,7 +2,7 @@ package org.muuvy.backend.business.rest;
 
 import org.muuvy.backend.business.rest.dto.FavouriteDto;
 import org.muuvy.backend.business.services.FavouriteService;
-
+import org.jboss.logging.Logger;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
@@ -11,6 +11,9 @@ import javax.ws.rs.core.Response;
 
 @Path("/user/{userId}/favourite")
 public class FavouriteController {
+    private static final Logger LOG = Logger.getLogger(FavouriteController.class);
+
+
     @Inject
     private FavouriteService favouriteService;
 
@@ -18,21 +21,46 @@ public class FavouriteController {
     private String userId;
 
 
+
+
+
+
     @GET
     @Produces("text/plain")
-    public Response doGet(){
-        return Response.ok(String.format("Hello %s from FavouriteService", userId)).build();
+    public Response doGet() {
+        try {
+            LOG.info(doGet());
+            return Response.ok(String.format("Hello %s from FavouriteService", userId)).build();
+        }catch (Exception e){
+            LOG.error(doGet());
+            return Response.serverError().build();
+        }
+
     }
+
 
     @POST
     public Response createFavourite(FavouriteDto favourite){
-       favouriteService.createFavourite(favourite);
-        return Response.ok().build();
+        try {
+            favouriteService.createFavourite(favourite);
+            LOG.info(favourite);
+            return Response.ok().build();
+        }catch (Exception e){
+           LOG.error(favourite);
+           return Response.serverError().build();
+        }
     }
 
     @DELETE
     public Response deleteFavourite(@QueryParam("movieId") String movieId){
-       favouriteService.deleteFavouriteId(movieId);
-        return Response.ok(String.format("deleted %s", movieId)).build();
+        try {
+            favouriteService.deleteFavouriteId(movieId);
+            LOG.info(movieId);
+            return Response.ok(String.format("deleted %s", movieId)).build();
+        }catch (Exception e){
+            LOG.error(movieId);
+            return Response.serverError().build();
+        }
+
     }
 }
--- src/main/java/org/muuvy/backend/business/rest/UserController.java
+++ src/main/java/org/muuvy/backend/business/rest/UserController.java
@@ -3,7 +3,7 @@ package org.muuvy.backend.business.rest;
 
 import org.muuvy.backend.business.rest.dto.UserDto;
 import org.muuvy.backend.business.services.UserService;
-
+import org.jboss.logging.Logger;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
@@ -12,24 +12,45 @@ import javax.ws.rs.core.Response;
 @Path("/user")
 public class UserController {
 
+	private static final Logger LOG = Logger.getLogger(UserController.class);
+
 	@Inject
 	private UserService userService;
 
 	@GET
 	@Produces("text/plain")
+
 	public Response doGet() {
-		return Response.ok("Hello from userController!").build();
+		try{
+			LOG.info(doGet());
+			return Response.ok("Hello from userController!").build();
+		}catch (Exception e){
+			LOG.error(doGet());
+			return Response.serverError().build();
+		}
 	}
 
 	@POST
 	public Response createUser(UserDto user){
-		userService.createUser(user);
-		return Response.ok().build();
+		try{
+			userService.createUser(user);
+			LOG.info(user);
+			return Response.ok().build();
+		}catch (Exception e){
+			LOG.error(user);
+			return Response.serverError().build();
+		}
 	}
 
 	@DELETE
 	public Response deleteUser(@QueryParam("userId") String userId){
-		userService.deleteById(userId);
-		return Response.ok(String.format("deleted %s", userId)).build();
+		try{
+			userService.deleteById(userId);
+			LOG.info(userId);
+			return Response.ok(String.format("deleted %s", userId)).build();
+		}catch (Exception e){
+			LOG.error(userId);
+			return Response.serverError().build();
+		}
 	}
 }
