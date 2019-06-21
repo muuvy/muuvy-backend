a345e4f30162f302e06c779387a6e6ecf996746d
Luca Hostettler <luca.hostettler@iseag.ch>
2019-06-21 14:24:55 +0200

    Fix FavoriteTest

--- src/test/java/org/muuvy/backend/persistence/models/FavoriteTest.java
+++ src/test/java/org/muuvy/backend/persistence/models/FavoriteTest.java
@@ -3,15 +3,15 @@ package org.muuvy.backend.persistence.models;
 import org.junit.jupiter.api.Test;
 import org.muuvy.backend.business.dao.FavoriteDAO;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class FavoriteTest {
 
 	@Test
 	public void createFavoriteTest() {
-		FavoriteDAO favoriteDAO = new FavoriteDAO();
-		favoriteDAO.create(new Favorite("movie123", "user123"));
-		assertNotNull(favoriteDAO.getAll());
+		Favorite favorite = new Favorite("movie123", "user123");
+		assertNotNull(favorite);
+		assertEquals("movie123", favorite.getId());
 	}
-
 }
